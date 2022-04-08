f7dd99bb78085f1632d7922e642d2de8d3fb12cc
KrispyWontonz <56931249+KrispyWontonz@users.noreply.github.com>
2022-04-08 10:02:03 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-function rangeValue(value){
-    const opacityValue = document.getElementById(opacitySlider);
-    range.style.setProperty('opacityValue', (100-value) / 100);
+function changeOpacity(val){
+    let circles = document.getElementById("circles");
+    circles.style.opacity=val;
 }
+
+function buttonChange(){
+    let spinning = document.getElementById("circles")
+    let running = spinning.style.animationPlayState === 'running'
+
+    spinning.style.animationPlayState = running ? 'running' : 'paused';
+}
+
+// const button = document.getElementById('button');
+// let spinning = document.getElementById('circles');
+// button.addEventListener('click');
+
+
